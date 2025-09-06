b7dbd63b320a1120ff0cdb1f22631a4e69048af2
ProCodeRxSatyam <satyamkumargaur3@gmail.com>
2025-09-07 02:00:44 +0530

    Added forget password option

---
+++ src/app/forgotpassword/page.jsx
@@ -0,0 +1,51 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+export default function Signup() {
+  const router = useRouter();
+
+  return (
+    <div className="blurframe">
+      <div className="frameborder relative flex flex-col hide-scrollbar  ">
+        <div className="w-[100%] bg-black h-[50px] flex sticky top-0 z-10 ">
+          <span
+            className=" text-3xl ml-[2%] mt-[1%]"
+            onClick={() => router.push("/")}
+          >
+            &#10005;
+          </span>
+          <div className="absolute left-1/2 top-[1%] -translate-x-1/2 mt-2">
+            <img
+              src="/TockLogo.png"
+              alt=""
+              className="w-[50px] bg-black rounded-md"
+            />
+          </div>
+        </div>
+
+        <form
+          action="/api/auth/signin"
+          method="POST"
+          className="py-15 px-20 flex flex-col gap-7 w-[100%] max-[712px]:py-45"
+        >
+          <h1 className="text-3xl font-semibold "> Find your Tock account</h1>
+          <p className="text-gray-600 ">Enter the email , associated with your account to change your Password</p>
+          <input
+            type="text"
+            placeholder="Email"
+            required
+            name="Username"
+            className="w-[100%] h-16 border border-gray-500 rounded-md pl-3  leading-[15rem] "
+                       
+          />
+
+          <button className="w-[100%] h-16 bg-gray-800 text-white rounded-full mt-[48%] font-semibold hover:bg-gray-500 max-[712]:pt-4 max-[712]:pb-4">
+            Next
+          </button>
+
+
+        </form>
+      </div>
+    </div>
+  );
+}
